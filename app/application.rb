bf71d2f1479c3bae5069f9faa21593eba48b0b00
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-31 01:42:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item = req.path.split("/songs/").last
     else
-      resp.write "Route not found"
+      resp.write "Item not found"
       resp.status = 404
     end
 
