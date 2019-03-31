80dd4f1c69ed7cde28ced5718c56b9bc9fbc73ac
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-31 01:42:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      song_title = req.path.split("/songs/").last
+      item = req.path.split("/songs/").last
     else
       resp.write "Route not found"
       resp.status = 404
